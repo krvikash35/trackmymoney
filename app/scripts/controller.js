d7ca35d3d151f2234409782ea62ce5fc7d9dcb63
Vikash <krvikash35@gmail.com>
2015-11-21 12:31:09 +0530

    added trx handler

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -75,18 +75,64 @@ controllerModule.controller('userInfoController', function($scope, $rootScope, $
 });
 
 
+
+
 controllerModule.controller('userTrxController', function($scope, $routeParams, $rootScope, $location, $http,  $window){
   $rootScope.isLandingPageVisible=false;
   var user="";
+  $scope.trxSource=[];
+  var trxDestination=[];
+
+  var trxUiHandler=function(trxType){
+    if(trxType === undefined || trxType === null){
+      trxType = 'Expense';
+    }
+
+  }
+
+
   $http.get("/user/"+$routeParams.userId+"/info")
   .success(function(data, status, headers, config){
     user=data.data;
-    $scope.trxSources=["demo","demo2"];
-    console.log("DebugUser1"+JSON.stringify(user));
+    user.moneyAccount.forEach(function(ma){
+      $scope.trxSource.push(ma.name);
+    });
+    // console.log("scopeTrxSource:"+$scope.trxSource);
+    // console.log("varTrxSource:"+trxSource);
+    // $scope.trxSource=trxSource;
+    $scope.trxDestination=user.sourceOfMoneyTrx.expenseSource;
+    $scope.trx.source=user.moneyAccount[0].name;
+    $scope.trx.destination=user.sourceOfMoneyTrx.expenseSource[0];
+    // console.log(trx.source);
+    // console.log("DebugUser1"+JSON.stringify(user));
   })
   .error(function(data, status, headers, config){
     $scope.error=data.data;
   });
+
+  $scope.trxTypeChanged = function(trxType){
+    console.log("debugChange: item changed");
+    switch (trxType) {
+      case 'Income':
+        $scope.trxSource=user.sourceOfMoneyTrx.incomeSource;
+        $scope.trxDestination=user.moneyAccount;
+        $scope.trx.source=user.sourceOfMoneyTrx.incomeSource[0];
+        $scope.trx.destination=user.moneyAccount[0];
+        console.log("debug:"+$scope.trx.source);
+        break;
+      case 'Transfer':
+        $scope.trxSource=user.moneyAccount;
+        $scope.trxDestination=user.moneyAccount;
+        $scope.trx.source=user.moneyAccount[0];
+        $scope.trx.destination=user.moneyAccount[0];
+        break;
+      default: //Expense
+        $scope.trxSource=user.moneyAccount;
+        $scope.trxDestination=user.sourceOfMoneyTrx.expenseSource;
+        $scope.trx.source=user.moneyAccount[0].name;
+        $scope.trx.destination=user.sourceOfMoneyTrx.expenseSource[0];
+    }
+  }
 
   // $rootScope.trxSources="";
   // $scope.submitTrxForm=function(trxForm){
