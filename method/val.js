cb5d56c529f03925e7d48b7759c17b3ea125df61
Vikash <krvikash35@gmail.com>
2015-11-26 00:00:41 +0530

    modified

--- method/val.js
+++ method/val.js
@@ -2,37 +2,28 @@ var valConfig = require('../config/val')
 var errConfig = require('../config/error')
 
 module.exports = {
-  valEmail: valEmail,
-  valPwd: valPwd,
-  valName: valName,
-  valMAcct: valMAcct,
-  valIncSrc: valIncSrc,
-  valExpSrc: valExpSrc,
-  valAmount: valAmount,
-  valTrxDesc: valTrxDesc
-}
 
 
-
-var valEmail = function(email){
+valEmail : function(email){
   var err;
   if(pwd === undefined || pwd == null || typeof(pwd) !== 'string' ||  email.length > valConfig.emailMaxLen || (!email.match(valConfig.emailRegex)) )
   return err=errConfig.E100;
-}
+},
 
-var valPwd = function(pwd){
+
+valPwd : function(pwd){
   var err;
   if( pwd === undefined || pwd === null || (!valConfig.pwdRegex.test(pwd)) )
   return err=errConfig.E101;
-}
+},
 
-var valName = function(name){
+valName : function(name){
   var err;
   if(name === undefined || name === null || (!valConfig.nameRegex.test(name)))
   return err=errConfig.E102;
-}
+},
 
-var valMAcct = function(ma){
+valMAcct : function(ma){
   var err;
   if( !(ma instanceof Array) || ma.length > valConfig.maMaxLen || ma.length < valConfig.maMinLen )
   return err=errConfig.E103;
@@ -42,9 +33,9 @@ var valMAcct = function(ma){
     if( ma[i].name === undefined || !valConfig.maNameRegex.test(ma[i].name) )
     return err=errConfig.E105;
   }
-}
+},
 
-var valIncSrc = function(iSrc){
+valIncSrc : function(iSrc){
   var err;
   if( !(iSrc instanceof Array) || iSrc.length > valConfig.iSrcMaxLen ||  iSrc.length < valConfig.iSrcMinLen )
   return err=errConfig.E106;
@@ -52,9 +43,9 @@ var valIncSrc = function(iSrc){
     if( iSrc[i] === undefined || !valConfig.iSrcRegex.test(iSrc[i]) )
     return err=errConfig.E107;
   }
-}
+},
 
-var valExpSrc = function(eSrc){
+valExpSrc : function(eSrc){
   var err;
   if( !(eSrc instanceof Array) || eSrc.length > valConfig.eSrcMaxLen ||
   eSrc.length < valConfig.eSrcMinLen )
@@ -63,18 +54,21 @@ var valExpSrc = function(eSrc){
     if( eSrc[i] === undefined || !valConfig.eSrcRegex.test(eSrc[i]) )
     return err=errConfig.E109;
   }
-}
+},
 
-var valAmount = function(amount){
+valAmount : function(amount){
   var err;
   if( amount === undefined || amount === null || !valConfig.amountRegex.test(amount) )
   return errConfig.E110;
-}
+},
 
-var valTrxDesc = function(desc){
+valTrxDesc : function(desc){
   var err;
   if(desc){
     if( desc.length > valConfig.trxDescMaxLen )
     return errConfig.E111;
   }
+}
+
+
 }
